dd03c6522d9e3a86b92fc6d53683bb352661e472
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-11 00:48:22 +0100

    Commenting!

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+
+    // Deleted code is debugged code!
+
+    /* Deleted code is 
+    debugged code! */
+    
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
